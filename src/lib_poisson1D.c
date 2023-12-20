214c436d90c463c543aa1b7335169a71edde17a8
“Yutai <yutai.zhao@gmail.com>
2023-12-20 10:55:59 +0100

    "Initial tp2 commit"

--- src/lib_poisson1D.c
+++ src/lib_poisson1D.c
@@ -4,161 +4,78 @@
 /* Poisson problem (Heat equation)            */
 /**********************************************/
 #include "lib_poisson1D.h"
-/*
- le stockage GB en priorit ́e colonne pour la matrice de Poisson 1D
- lab : row, la : column, AB : matrice Poisson 1D, kv : num of sup/low diag
- */
-void set_GB_operator_colMajor_poisson1D(double* AB, int *lab, int *la, int *kv) {
-    for (int j = 0; j < (*la); ++j) {
-        for (int i = 0; i < 2 * (*kv) + 1; ++i) {
-            if (i == 2 * (*kv) - 1) {
-                AB[i + j * (2 * (*kv) + 1)] = -2;
-            } else {
-                AB[i + j * (2 * (*kv) + 1)] = 1;
-            }
-            printf("%f ", AB[i + j * (2 * (*kv) + 1)] );
-        }
-        printf("\n");
-    }
-    AB[0] = 0;
-    AB[((*la) * (2 * (*kv) + 1)) - 1] = 0;
-}
 
+void set_GB_operator_colMajor_poisson1D(double* AB, int *lab, int *la, int *kv){
+  int ii, jj, kk;
+  for (jj=0;jj<(*la);jj++){
+    kk = jj*(*lab);
+    if (*kv>=0){
+      for (ii=0;ii< *kv;ii++){
+	AB[kk+ii]=0.0;
+      }
+    }
+    AB[kk+ *kv]=-1.0;
+    AB[kk+ *kv+1]=2.0;
+    AB[kk+ *kv+2]=-1.0;
+  }
+  AB[0]=0.0;
+  if (*kv == 1) {AB[1]=0;}
+  
+  AB[(*lab)*(*la)-1]=0.0;
+}
 
 void set_GB_operator_colMajor_poisson1D_Id(double* AB, int *lab, int *la, int *kv){
-    for (int j = 0; j < (*la); ++j) {
-        for (int i = 0; i < 2 * (*kv) + 1; ++i) {
-            if (i == 2 * (*kv) - 1) {
-                AB[i + j * (2 * (*kv) + 1)] = 1;
-            } else {
-                AB[i + j * (2 * (*kv) + 1)] = 0;
-            }
-        }
+  int ii, jj, kk;
+  for (jj=0;jj<(*la);jj++){
+    kk = jj*(*lab);
+    if (*kv>=0){
+      for (ii=0;ii< *kv;ii++){
+	AB[kk+ii]=0.0;
+      }
     }
+    AB[kk+ *kv]=0.0;
+    AB[kk+ *kv+1]=1.0;
+    AB[kk+ *kv+2]=0.0;
+  }
+  AB[1]=0.0;
+  AB[(*lab)*(*la)-1]=0.0;
 }
 
-//vectur initial b
 void set_dense_RHS_DBC_1D(double* RHS, int* la, double* BC0, double* BC1){
-    int g  = 0 ;
-    
-    for (int i = 1; i < (*la)-1 ; ++i){
-        RHS[i] = g;
-    }
-    RHS[0] = *BC0;
-    RHS[*la-1] = *BC1;
-    
-}
+  int jj;
+  RHS[0]= *BC0;
+  RHS[(*la)-1]= *BC1;
+  for (jj=1;jj<(*la)-1;jj++){
+    RHS[jj]=0.0;
+  }
+}  
 
 void set_analytical_solution_DBC_1D(double* EX_SOL, double* X, int* la, double* BC0, double* BC1){
-    
-    for (int i = 0; i < (*la) ; ++i){
-        EX_SOL[i] = *BC0 + X[i]*(*BC1 - *BC0);
-    }
-    
-}
+  int jj;
+  double h, DELTA_T;
+  DELTA_T=(*BC1)-(*BC0);
+  for (jj=0;jj<(*la);jj++){
+    EX_SOL[jj] = (*BC0) + X[jj]*DELTA_T;
+  }
+}  
 
 void set_grid_points_1D(double* x, int* la){
-    for (int i = 0; i < *la; ++i)
-          {
-            x[i] = i / (*la - 1);
-          }
-}
-
-void write_GB_operator_rawMajor_poisson1D(double* AB, int* lab, int* la, char* filename){
-  FILE * file;
-  int ii,jj;
-  file = fopen(filename, "w");
-  //Numbering from 1 to la
-  if (file != NULL){
-    for (ii=0;ii<(*lab);ii++){
-      for (jj=0;jj<(*la);jj++){
-	fprintf(file,"%lf\t",AB[ii*(*la)+jj]);
-      }
-      fprintf(file,"\n");
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  }
-}
-
-void write_GB_operator_colMajor_poisson1D(double* AB, int* lab, int* la, char* filename){
-  FILE * file;
-  int ii,jj;
-  file = fopen(filename, "w");
-  //Numbering from 1 to la
-  if (file != NULL){
-    for (ii=0;ii<(*la);ii++){
-      for (jj=0;jj<(*lab);jj++){
-	fprintf(file,"%lf\t",AB[ii*(*lab)+jj]);
-      }
-      fprintf(file,"\n");
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  }
-}
-
-void write_GB2AIJ_operator_poisson1D(double* AB, int* la, char* filename){
-  FILE * file;
   int jj;
-  file = fopen(filename, "w");
-  //Numbering from 1 to la
-  if (file != NULL){
-    for (jj=1;jj<(*la);jj++){
-      fprintf(file,"%d\t%d\t%lf\n",jj,jj+1,AB[(*la)+jj]);
-    }
-    for (jj=0;jj<(*la);jj++){
-      fprintf(file,"%d\t%d\t%lf\n",jj+1,jj+1,AB[2*(*la)+jj]);
-    }
-    for (jj=0;jj<(*la)-1;jj++){
-      fprintf(file,"%d\t%d\t%lf\n",jj+2,jj+1,AB[3*(*la)+jj]);
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
+  double h;
+  h=1.0/(1.0*((*la)+1));
+  for (jj=0;jj<(*la);jj++){
+    x[jj]=(jj+1)*h;
   }
 }
 
-void write_vec(double* vec, int* la, char* filename){
-  int jj;
-  FILE * file;
-  file = fopen(filename, "w");
-  // Numbering from 1 to la
-  if (file != NULL){
-    for (jj=0;jj<(*la);jj++){
-      fprintf(file,"%lf\n",vec[jj]);
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  } 
-}  
-
-void write_xy(double* vec, double* x, int* la, char* filename){
-  int jj;
-  FILE * file;
-  file = fopen(filename, "w");
-  // Numbering from 1 to la
-  if (file != NULL){
-    for (jj=0;jj<(*la);jj++){
-      fprintf(file,"%lf\t%lf\n",x[jj],vec[jj]);
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  } 
-}  
-
-int indexABCol(int i, int j, int *lab){
+double relative_forward_error(double* x, double* y, int* la){
   return 0;
 }
+
+int indexABCol(int i, int j, int *lab){
+  return j*(*lab)+i;
+}
+
 int dgbtrftridiag(int *la, int*n, int *kl, int *ku, double *AB, int *lab, int *ipiv, int *info){
   return *info;
 }
